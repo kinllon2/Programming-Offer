d49597211f6cc4c80ca6c359891e94c110d12bb8
WangChengtao <821997225@qq.com>
2019-02-12 12:27:38 +0800

    finish question2 add a function about multipy

--- pinduoduo002/pinduoduo002/pinduoduo002/Source.cpp
+++ pinduoduo002/pinduoduo002/pinduoduo002/Source.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<sstream>
 #include<string>
 #include<vector>
 #include<deque>
 #include<algorithm>
 #include<math.h>
+#
 using namespace std;
 
 string BigNumMultiply(string s1,string s2)
@@ -17,6 +19,7 @@ string BigNumMultiply(string s1,string s2)
 			vec[i+j]+= (s1[i] - '0')*(s2[j] - '0');
 	}
 	//进位处理
+	//从最高位开始计算进位值，从后往前进行清算
 	int addflag = 0;
 	for (int i = vec.size() - 1; i >= 0; --i)
 	{
@@ -24,6 +27,20 @@ string BigNumMultiply(string s1,string s2)
 		vec[i] = temp % 10;//当前值
 		addflag = temp / 10;//进位值
 	}
+
+
+	//判断最后的一个进位标志，如果有进位，添加到队列的最前面
+	while (addflag != 0) {
+		int t = addflag % 10;
+		vec.push_front(t);
+		addflag /= 10;
+	}
+	for (auto c : vec) {
+		ostringstream ss;
+		ss << c;
+		res = res + ss.str();
+	}
+	return res;
 
 }
 
@@ -34,8 +51,9 @@ int main()
 	string str1,str2;
 	while(cin>>str1>>str2)
 	{
-		cout << str1 << "*" << str2 << "=" << std::endl;
-		cout << BigNumMultiply(str1, str2) << std::endl;
+		//cout << str1 << "*" << str2 << "=" << std::endl;
+		///cout << BigNumMultiply(str1, str2) << std::endl;
+		cout << BigNumMultiply(str1, str2) << endl;
 	}
 	
 	
