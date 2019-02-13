3319a862466e33361d1dc37f19ec8355daea548b
WangChengtao <821997225@qq.com>
2019-02-13 16:26:43 +0800

    []create a simply function about bfs but still have some question

--- pinduoduo004/pinduoduo004/pinduoduo004/Source.cpp
+++ pinduoduo004/pinduoduo004/pinduoduo004/Source.cpp
@@ -33,7 +33,41 @@ int bfs(vector<vector<char>>& a, int startx, int starty)
 	vector<vector<int>> dir{ { -1,0 },{ 1,0 },{ 0,1 },{ 0,-1 } };//移动方式
 	int visit[105][105][1200] = { 0 };
 
+	while(que.empty() ==false)
+	{
+		auto top = que.front();
+		que.pop();
 
+
+		for (auto d : dir)
+		{
+			//撞墙
+			int x = top.x + d[0], y = top.y + d[1];
+			if (x < 0 || x >= a.size() || y < 0 || y >= a[0].size() || a[x][y] == '0')
+				continue;
+
+			//遇到钥匙去做添加
+			int key = top.key;
+			if (a[x][y] >= 'a' && a[x][y] <= 'z')
+				key = (key | (1 << (a[x][y] - 'a')));
+
+
+			//遇到门去找钥匙
+			if (a[x][y] >= 'A' && a[x][y] <= 'Z' && (key&(1 << (a[x][y] - 'A'))) == 0)
+				continue;
+
+
+			//在x和y位置的地方由于key是否访问过
+			if (visit[x][y][key] == 0)
+			{
+				visit[x][y][key] = 1;
+				que.push(Point(x, y, key, top.step + 1));
+			}
+
+
+
+		}
+	}
 
 
 }
